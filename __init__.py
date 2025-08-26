3557185b9a1edfe0907ca6e629f74d537ed14f9f
tl2309 <144661509+tl2309@users.noreply.github.com>
2025-08-26 23:10:08 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+from . import wiki_utils
+# from . import transformation
+from . import qageneration
+
+
+# class Transfer:
+#     def trans(self, path):
+#         pass
